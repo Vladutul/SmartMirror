f906c56b84fa50a56a9c5bb044ce1f9a07a84cdd
Vladutul <65509208+Vladutul@users.noreply.github.com>
2026-03-03 13:06:16 +0200

    0.0.0.2

--- start_up.py
+++ start_up.py
@@ -1,22 +1,14 @@
-from picam2 import Picamera2
+from picamera2 import Picamera2
 import time
 
-# Inițializăm camera
 picam2 = Picamera2()
-
-# Configurăm camera pentru previzualizare
 picam2.configure(picam2.create_preview_configuration())
 
-print("Pornesc camera pe Trixie...")
-
-# Pornim camera și previzualizarea pe ecran
+print("Pornesc feed-ul video pe monitor...")
 picam2.start()
-picam2.start_preview()
 
-# Lăsăm previzualizarea 10 secunde
+# Lăsăm imaginea pe ecran 10 secunde
 time.sleep(10)
 
-# Oprim camera
-picam2.stop_preview()
 picam2.stop()
-print("Camera a fost oprită.")
+print("S-a închis.")
