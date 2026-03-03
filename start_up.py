007b789ced0e9d5cb3c90f65bfaaae521d08a87e
Vladutul <65509208+Vladutul@users.noreply.github.com>
2026-03-03 13:11:35 +0200

    0.0.0.3

--- start_up.py
+++ start_up.py
@@ -1,14 +1,24 @@
-from picamera2 import Picamera2
+from picamera2 import Picamera2, Preview
 import time
 
 picam2 = Picamera2()
-picam2.configure(picam2.create_preview_configuration())
 
-print("Pornesc feed-ul video pe monitor...")
+# Setăm configurația
+config = picam2.create_preview_configuration(main={"size": (800, 600)})
+picam2.configure(config)
+
+print("Deschid fereastra video pe ecran...")
+
+# ASTA E LINIA MAGICĂ CARE LIPSEA:
+picam2.start_preview(Preview.QTGL)
+
+# Pornim captura
 picam2.start()
 
-# Lăsăm imaginea pe ecran 10 secunde
+# Lăsăm fereastra pe ecran 10 secunde
 time.sleep(10)
 
+# Oprim totul curat
+picam2.stop_preview()
 picam2.stop()
 print("S-a închis.")
