db01c409a53fc3dbf9f8815ec09c5659c0e010ff
Vladutul <65509208+Vladutul@users.noreply.github.com>
2026-03-03 13:19:39 +0200

    0.0.0.4

--- start_up.py
+++ start_up.py
@@ -3,19 +3,22 @@ import time
 
 picam2 = Picamera2()
 
+# Setăm configurația
 config = picam2.create_preview_configuration(main={"size": (800, 600)})
 picam2.configure(config)
 
-print("Trimit fluxul video direct pe hardware-ul ecranului (DRM)...")
+print("Deschid fereastra video pe ecran...")
 
-# Aici e magia: folosim DRM în loc de QTGL
-picam2.start_preview(Preview.DRM)
+# ASTA E LINIA MAGICĂ CARE LIPSEA:
+picam2.start_preview(Preview.QTGL)
 
+# Pornim captura
 picam2.start()
 
-# Lăsăm pe ecran 10 secunde
+# Lăsăm fereastra pe ecran 10 secunde
 time.sleep(10)
 
+# Oprim totul curat
 picam2.stop_preview()
 picam2.stop()
 print("S-a închis.")
