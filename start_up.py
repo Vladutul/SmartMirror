7c7a4dcfec3c80e3c96590cd7165be80afbfbbbb
Vladutul <65509208+Vladutul@users.noreply.github.com>
2026-03-03 12:44:18 +0200

    0.0.0.0

--- start_up.py
+++ start_up.py
@@ -1,1 +1,21 @@
-main 
+from picamera import PiCamera
+from time import sleep
+
+# Inițializăm camera
+camera = PiCamera()
+
+# Opțional: Dacă imaginea este cu susul în jos, poți roti camera
+# camera.rotation = 180
+
+print("Pornesc camera...")
+
+# Pornim previzualizarea pe ecran
+camera.start_preview()
+
+# Lăsăm previzualizarea activă timp de 10 secunde
+# În acest timp vei vedea live pe monitor ce vede camera
+sleep(10)
+
+# Oprim previzualizarea
+camera.stop_preview()
+print("Camera a fost oprită.")
